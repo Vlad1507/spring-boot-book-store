2f152bb08f2945b8405a2a3de1b1aae3682d95f9
Vlad Andriichuk <vlad150096@gmail.com>
2024-10-28 23:48:04 +0100

    Changed minor details

--- src/main/java/com/store/bookstore/BookStoreApplication.java
+++ src/main/java/com/store/bookstore/BookStoreApplication.java
@@ -1,7 +1,5 @@
 package com.store.bookstore;
 
-import com.store.bookstore.services.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
