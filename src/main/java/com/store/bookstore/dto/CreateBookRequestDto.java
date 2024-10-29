f65df7144684f88cc0ad69f695bd3a46901608be
Vlad Andriichuk <vlad150096@gmail.com>
2024-10-29 10:14:11 +0100

    Changed minor details. Changed spring-boot-starter-parent version from 3.4.0-M3 to 3.3.5.

--- src/main/java/com/store/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/com/store/bookstore/dto/CreateBookRequestDto.java
@@ -1,11 +1,9 @@
 package com.store.bookstore.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class CreateBookRequestDto {
     private String title;
     private String author;
