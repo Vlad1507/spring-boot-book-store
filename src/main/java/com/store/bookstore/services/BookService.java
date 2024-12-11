441771d1853f7b55ff05fb35c6bd0d50947500af
Vlad Andriichuk <vlad150096@gmail.com>
2024-12-11 13:05:10 +0100

    Added to controller's method getAll() ability to show data by page. Added dependency for openapi swagger ui available at URL http://localhost:8080/swagger-ui/index.html

--- src/main/java/com/store/bookstore/services/BookService.java
+++ src/main/java/com/store/bookstore/services/BookService.java
@@ -4,11 +4,12 @@ import com.store.bookstore.dto.BookDto;
 import com.store.bookstore.dto.BookSearchParametersDto;
 import com.store.bookstore.dto.CreateBookRequestDto;
 import java.util.List;
+import org.springframework.data.domain.Pageable;
 
 public interface BookService {
     BookDto save(CreateBookRequestDto requestDto);
 
-    List<BookDto> findAll();
+    List<BookDto> findAll(Pageable pageable);
 
     BookDto findById(Long id);
 
